1d651c1d0a48842e90b568817b4db76c43a0bb4d
phamlamtruongag@gmail.com <phamlamtruongag@gmail.com>
2020-05-26 20:00:46 +0700

    cc

---
+++ routes/bai1.router.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var Router = express();
+const fs = require("fs");
+
+Router.get("/", function (req, res) {
+  fs.readFile("Products.txt", "utf8", function (err, data) {
+    if (err) throw err;
+    let arrdata = data.split("\n");
+    let objectdata;
+    let listSanPham = [];
+    let count = 0;
+    arrdata.forEach((element) => {
+      let sanpham = element.split("|");
+      if (!sanpham[0]) return res.status(200).send(listSanPham);
+      objectdata = {
+        IDSanPham: sanpham[0],
+        TenSanPham: sanpham[1],
+        Gia: sanpham[2],
+        ThuongHieu: sanpham[3],
+        SoLuong: sanpham[4],
+        Mota: sanpham[5],
+        Hinhanh: sanpham[6],
+      };
+
+      listSanPham.push(objectdata);
+    });
+    //listSanPham.push(objectdata);
+    //return res.status(200).send(listSanPham);
+  });
+});
+
+module.exports = Router;
