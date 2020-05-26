6fc1000b106b5798351cd85dd67dc86ff6469b90
phamlamtruongag@gmail.com <phamlamtruongag@gmail.com>
2020-05-26 20:23:38 +0700

    1

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 var indexRouter = require("./routes/index");
-const port = 9000;
 
 app.get("/", function (req, res) {
   res.status(200).send("Hello world!");
 });
 app.use(express.static("public"));
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+app.listen(9000, () => {
+  console.log("server is running on port 9000");
+});
