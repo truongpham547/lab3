508a30ff7608e6e3c3037497554efadc649f2c24
phamlamtruongag@gmail.com <phamlamtruongag@gmail.com>
2020-05-26 22:26:22 +0700

    1

--- routes/bai1.router.js
+++ routes/bai1.router.js
@@ -3,30 +3,30 @@ var Router = express();
 const fs = require("fs");
 
 Router.get("/", function (req, res) {
-  fs.readFile("Products.txt", "utf8", function (err, data) {
-    if (err) throw err;
-    let arrdata = data.split("\n");
-    let objectdata;
-    let listSanPham = [];
-    let count = 0;
-    arrdata.forEach((element) => {
-      let sanpham = element.split("|");
-      if (!sanpham[0]) return res.status(200).send(listSanPham);
-      objectdata = {
-        IDSanPham: sanpham[0],
-        TenSanPham: sanpham[1],
-        Gia: sanpham[2],
-        ThuongHieu: sanpham[3],
-        SoLuong: sanpham[4],
-        Mota: sanpham[5],
-        Hinhanh: sanpham[6],
-      };
-
-      listSanPham.push(objectdata);
+  try {
+    fs.readFile("Products.txt", "utf8", function (err, data) {
+      if (err) throw err;
+      let arrdata = data.split("\n");
+      let objectdata;
+      let listSanPham = [];
+      arrdata.forEach((element) => {
+        let sanpham = element.split("|");
+        if (!sanpham[0]) return res.status(200).send(listSanPham);
+        objectdata = {
+          IDSanPham: sanpham[0],
+          TenSanPham: sanpham[1],
+          Gia: sanpham[2],
+          ThuongHieu: sanpham[3],
+          SoLuong: sanpham[4],
+          Mota: sanpham[5],
+          Hinhanh: sanpham[6],
+        };
+        listSanPham.push(objectdata);
+      });
     });
-    //listSanPham.push(objectdata);
-    //return res.status(200).send(listSanPham);
-  });
+  } catch (error) {
+    return res.status(200).send(error);
+  }
 });
 
 module.exports = Router;
